27b6dcfdcf23cac4cfd1ba8a21064840e172a43c
Noahkimtai <noahkimtai24@gmail.com>
2022-09-03 00:01:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name,mail){
+    let bodyobj= {
+        name:name,
+        email:mail
+    };
+    return fetch('http://localhost:3002/users',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            accept:'application/json',
+        },
+        body:JSON.stringify(bodyobj)
+    }).then(res =>res.json()).then(data =>{
+        let p = document.querySelector('p')
+        p.innerText = data.id
+        document.appendChild(p)
+    });
+};
+console.log(submitData('noah','noah.com'))
